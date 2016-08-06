b1d2bc214350038f12c508458277f93df8e03cac
mlustig <mlustig@google.com>
2016-08-06 18:18:37 -0400

    S04.01-Exercise-LaunchNewActivity

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -36,7 +36,6 @@ import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
 
-// COMPLETED (8) Implement ForecastAdapterOnClickHandler from the MainActivity
 public class MainActivity extends AppCompatActivity implements ForecastAdapterOnClickHandler {
 
     private RecyclerView mRecyclerView;
@@ -76,7 +75,6 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
          */
         mRecyclerView.setHasFixedSize(true);
 
-        // COMPLETED (11) Pass in 'this' as the ForecastAdapterOnClickHandler
         /*
          * The ForecastAdapter is responsible for linking our weather data with the Views that
          * will end up displaying our weather data.
@@ -110,8 +108,6 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         new FetchWeatherTask().execute(location);
     }
 
-    // COMPLETED (9) Override ForecastAdapterOnClickHandler's onClick method
-    // COMPLETED (10) Show a Toast when an item is clicked, displaying that item's weather data
     /**
      * This method is overridden by our MainActivity class in order to handle RecyclerView item
      * clicks.
@@ -121,6 +117,9 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
     @Override
     public void onClick(String weatherForDay) {
         Context context = this;
+        // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
+        // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
+        // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
         Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
                 .show();
     }
