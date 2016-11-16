513bcb3d5cfe3946aaa265390788a988dcc0cbbc
Michael Lustig <halfjew22@gmail.com>
2016-11-16 16:40:26 -0500

    S06.02-Solution-SettingsFragment

--- app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -20,7 +20,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 /**
- * Loads the SettingsFragment and handles the proper behavior of the up button.
+ * SettingsActivity is responsible for displaying the {@link SettingsFragment}. It is also
+ * responsible for orchestrating proper navigation when the up button is clicked. When the up
+ * button is clicked from the SettingsActivity, we want to navigate to the Activity that the user
+ * came from to get to the SettingsActivity.
+ * <p>
+ * For example, when the user is in the DetailActivity and clicks the settings option in the menu,
+ * and then clicks the up button, we want to navigate back to the DetailActivity. If the user
+ * navigates to the SettingsActivity from the MainActivity, then we want to navigate back to the
+ * MainActivity when the user clicks the up button from the SettingsActivity.
  */
 public class SettingsActivity extends AppCompatActivity {
 
@@ -29,27 +37,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_settings);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // TODO (2) Create an xml resource directory
-        // TODO (3) Add a PreferenceScreen with an EditTextPreference and ListPreference within the newly created xml resource directory
-
-        // TODO (4) Create SettingsFragment and extend PreferenceFragmentCompat
-
-        // Do steps 5 - 11 within SettingsFragment
-        // TODO (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
-
-        // TODO (8) Create a method called setPreferenceSummary that accepts a Preference and an Object and sets the summary of the preference
-
-        // TODO (5) Override onCreatePreferences and add the preference xml file using addPreferencesFromResource
-
-        // Do step 9 within onCreatePreference
-        // TODO (9) Set the preference summary on each preference that isn't a CheckBoxPreference
-
-        // TODO (13) Unregister SettingsFragment (this) as a SharedPreferenceChangedListener in onStop
-
-        // TODO (12) Register SettingsFragment (this) as a SharedPreferenceChangedListener in onStart
-
-        // TODO (11) Override onSharedPreferenceChanged to update non CheckBoxPreferences when they are changed
     }
 
     @Override
