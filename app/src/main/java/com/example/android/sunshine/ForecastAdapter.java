9f73cf78073eaf71b588c8118005ad44c01fde7c
Michael Lustig <halfjew22@gmail.com>
2016-11-16 16:42:58 -0500

    S11.02-Exercise-TodayListItem

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -34,6 +34,14 @@ import com.example.android.sunshine.utilities.SunshineWeatherUtils;
  */
 class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
 
+//  TODO (1) Add a layout called list_item_forecast_today
+//  TODO (2) Using ConstraintLayout, implement the today list item layout
+
+//  TODO (4) Create a resources file called bools.xml within the res/values-port directory
+//  TODO (5) Within bools.xml in the portrait specific directory, add a bool called use_today_layout and set it to false
+
+//  TODO (6) Declare constant IDs for the ViewType for today and for a future day
+
     /* The context we use to utility methods, app resources and layout inflaters */
     private final Context mContext;
 
@@ -58,6 +66,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
      * is in landscape. This flag will be set in the constructor of the adapter by accessing
      * boolean resources.
      */
+//  TODO (7) Declare a private boolean called mUseTodayLayout
 
     private Cursor mCursor;
 
@@ -71,6 +80,7 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
     public ForecastAdapter(@NonNull Context context, ForecastAdapterOnClickHandler clickHandler) {
         mContext = context;
         mClickHandler = clickHandler;
+//      TODO (8) Set mUseTodayLayout to the value specified in resources
     }
 
     /**
@@ -86,6 +96,12 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
      */
     @Override
     public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+
+//      TODO (12) If the view type of the layout is today, use today layout
+
+//      TODO (13) If the view type of the layout is future day, use future day layout
+
+//      TODO (14) Otherwise, throw an IllegalArgumentException
 
         View view = LayoutInflater
                 .from(mContext)
@@ -108,13 +124,17 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
     public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
         mCursor.moveToPosition(position);
 
-//      COMPLETED (7) Replace the single TextView with Views to display all of the weather info
-
         /****************
          * Weather Icon *
          ****************/
         int weatherId = mCursor.getInt(MainActivity.INDEX_WEATHER_CONDITION_ID);
         int weatherImageId;
+
+//      TODO (15) If the view type of the layout is today, display a large icon
+
+//      TODO (16) If the view type of the layout is future day, display a small icon
+
+//      TODO (17) Otherwise, throw an IllegalArgumentException
 
         weatherImageId = SunshineWeatherUtils
                 .getSmallArtResourceIdForWeatherCondition(weatherId);
@@ -191,6 +211,10 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
         return mCursor.getCount();
     }
 
+//  TODO (9) Override getItemViewType
+//      TODO (10) Within getItemViewType, if mUseTodayLayout is true and position is 0, return the ID for today viewType
+//      TODO (11) Otherwise, return the ID for future day viewType
+
     /**
      * Swaps the cursor used by the ForecastAdapter for its weather data. This method is called by
      * MainActivity after a load has finished, as well as when the Loader responsible for loading
@@ -210,20 +234,15 @@ class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapt
      * OnClickListener, since it has access to the adapter and the views.
      */
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final ImageView iconView;
 
-//      COMPLETED (4) Replace the weatherSummary TextView with individual weather detail TextViews
         final TextView dateView;
         final TextView descriptionView;
         final TextView highTempView;
         final TextView lowTempView;
 
-//      COMPLETED (5) Add an ImageView for the weather icon
-        final ImageView iconView;
-
         ForecastAdapterViewHolder(View view) {
             super(view);
-
-//          COMPLETED (6) Get references to all new views and delete this line
 
             iconView = (ImageView) view.findViewById(R.id.weather_icon);
             dateView = (TextView) view.findViewById(R.id.date);
