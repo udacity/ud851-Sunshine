8de57ae6a9d628498e80abb170fb06588940d9aa
Michael Lustig <halfjew22@gmail.com>
2016-11-16 16:39:57 -0500

    S06.02-Exercise-SettingsFragment

--- app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -19,7 +19,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-// COMPLETED (1) Add new Activity called SettingsActivity using Android Studio wizard
 /**
  * Loads the SettingsFragment and handles the proper behavior of the up button.
  */
@@ -29,8 +28,28 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_settings);
-        // COMPLETED (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // TODO (2) Create an xml resource directory
+        // TODO (3) Add a PreferenceScreen with an EditTextPreference and ListPreference within the newly created xml resource directory
+
+        // TODO (4) Create SettingsFragment and extend PreferenceFragmentCompat
+
+        // Do steps 5 - 11 within SettingsFragment
+        // TODO (10) Implement OnSharedPreferenceChangeListener from SettingsFragment
+
+        // TODO (8) Create a method called setPreferenceSummary that accepts a Preference and an Object and sets the summary of the preference
+
+        // TODO (5) Override onCreatePreferences and add the preference xml file using addPreferencesFromResource
+
+        // Do step 9 within onCreatePreference
+        // TODO (9) Set the preference summary on each preference that isn't a CheckBoxPreference
+
+        // TODO (13) Unregister SettingsFragment (this) as a SharedPreferenceChangedListener in onStop
+
+        // TODO (12) Register SettingsFragment (this) as a SharedPreferenceChangedListener in onStart
+
+        // TODO (11) Override onSharedPreferenceChanged to update non CheckBoxPreferences when they are changed
     }
 
     @Override
