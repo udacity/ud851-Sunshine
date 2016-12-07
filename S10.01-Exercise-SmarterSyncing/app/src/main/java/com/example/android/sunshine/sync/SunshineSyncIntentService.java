4248c217e57d83c27f8c797532186235fef68c0a
Jeremy Silver <jeremy@udacity.com>
2016-12-06 23:15:20 -0800

    Remove duplicate session 10 folders

--- S10.01-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++
@@ -1,3 +0,0 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
-//  TODO (6) Create a constructor that calls super and passes the name of this class
-//  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
