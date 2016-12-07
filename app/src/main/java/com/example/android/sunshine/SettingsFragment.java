3f2834c1c695ab1927a8aaae469584bfd1a73c85
Asser Samak <asser@asamak.local>
2016-12-06 18:54:22 -0800

    S10.01-Solution-SynchronizingTheWeather

--- app/src/main/java/com/example/android/sunshine/SettingsFragment.java
+++ app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -26,6 +26,7 @@ import android.support.v7.preference.PreferenceScreen;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.sync.SunshineSyncUtils;
 
 /**
  * The SettingsFragment serves as the display for all of the user's settings. In Sunshine, the
@@ -97,7 +98,8 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             // we've changed the location
             // Wipe out any potential PlacePicker latlng values so that we can use this text entry.
             SunshinePreferences.resetLocationCoordinates(activity);
-            //  TODO (14) Sync the weather if the location changes
+            //  COMPLETED (14) Sync the weather if the location changes
+            SunshineSyncUtils.startImmediateSync(activity);
         } else if (key.equals(getString(R.string.pref_units_key))) {
             // units have changed. update lists of weather entries accordingly
             activity.getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
