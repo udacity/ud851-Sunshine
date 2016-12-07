1bef4a9c39cf16d64e804381f59c390a0a73ed24
Asser Samak <asser@asamak.local>
2016-12-06 18:54:48 -0800

    S10.02-Exercise-SmarterSyncing

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -25,10 +25,8 @@ import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
 
 import java.net.URL;
 
-//  COMPLETED (1) Create a class called SunshineSyncTask
 public class SunshineSyncTask {
 
-//  COMPLETED (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
 
     /**
      * Performs the network request for updated weather, parses the JSON from that request, and
@@ -40,7 +38,6 @@ public class SunshineSyncTask {
      */
     synchronized public static void syncWeather(Context context) {
 
-//      COMPLETED (3) Within syncWeather, fetch new weather data
 
         try {
             /*
@@ -67,7 +64,6 @@ public class SunshineSyncTask {
                 /* Get a handle on the ContentResolver to delete and insert data */
                 ContentResolver sunshineContentResolver = context.getContentResolver();
 
-//              COMPLETED (4) If we have valid results, delete the old data and insert the new
                 /* Delete old weather data because we don't need to keep multiple days' data */
                 sunshineContentResolver.delete(
                         WeatherContract.WeatherEntry.CONTENT_URI,
