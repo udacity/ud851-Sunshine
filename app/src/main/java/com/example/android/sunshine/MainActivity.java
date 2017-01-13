a33d77b9b3d75814e0e7bf5c4f012c7bd4125016
Jeremy Silver <jeremy@udacity.com>
2017-01-12 18:08:21 -0800

    S01.02-Exercise-AddWeatherList

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -20,9 +20,17 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO (1) Create a field to store the weather display TextView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        // TODO (2) Use findViewById to get a reference to the weather display TextView
+
+        // TODO (3) Create an array of Strings that contain fake weather data
+
+        // TODO (4) Append each String from the fake weather data array to the TextView
     }
 }
