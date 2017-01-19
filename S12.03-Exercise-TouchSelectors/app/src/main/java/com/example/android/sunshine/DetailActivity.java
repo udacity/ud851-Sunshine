59d90e9442438482df9336c47b35513f047edc8b
Jeremy Silver <jeremy@udacity.com>
2017-01-19 15:13:25 -0800

    Fix issue #13

--- S12.03-Exercise-TouchSelectors/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S12.03-Exercise-TouchSelectors/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -127,7 +127,7 @@ public class DetailActivity extends AppCompatActivity implements
         /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
         MenuInflater inflater = getMenuInflater();
         /* Use the inflater's inflate method to inflate our menu layout to this menu */
-        inflater.inflate(R.menu.forecast, menu);
+        inflater.inflate(R.menu.detail, menu);
         /* Return true so that the menu is displayed in the Toolbar */
         return true;
     }
