eb781394fb7b822166636551c5789f89a85287ee
Russell Elfenbein <russell.elfenbein@gmail.com>
2017-02-02 05:44:14 +0000

    Remove TODO #20
    Remove //TODO #20 Call showWeatherDataView before executing the AsyncTask
    
    Showing mWeatherTextView is handled in FetchWeatherTask#onPostExectute.

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -61,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
      * background method to get the weather data in the background.
      */
     private void loadWeatherData() {
-        // TODO (20) Call showWeatherDataView before executing the AsyncTask
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
     }
