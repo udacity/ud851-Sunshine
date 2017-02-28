28e160814de93d6eaec2ebfba20110b1faf1edde
egmp777 <egmp777@gmail.com>
2017-02-28 18:06:45 -0500

    Done
    Exercise S03.01

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -251,8 +251,8 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_refresh) {
 
             // TODO (46) Instead of setting the text to "", set the adapter to null before refreshing
+
             mForecastAdapter.setWeatherData(null);
-            
             loadWeatherData();
             return true;
         }
