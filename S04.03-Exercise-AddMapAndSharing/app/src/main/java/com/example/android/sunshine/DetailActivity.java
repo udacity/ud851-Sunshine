937a815de8861dca6550a0b0606a93e381821d3e
egmp777 <egmp777@gmail.com>
2017-03-02 17:25:18 -0500

    S04.03-Exercise-AddMapAndSharing
    Done - Sharing box does not show either shared data nor the title????

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,10 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -30,5 +33,21 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     // TODO (3) Create a menu with an item with id of action_share
+
     // TODO (4) Display the menu and implement the forecast sharing functionality
+    private Intent buildIntent(String type){
+        Intent intent = ShareCompat.IntentBuilder.from(this)
+                .setType(type)
+                .setText(mForecast + FORECAST_SHARE_HASHTAG)
+                .getIntent();
+        return intent;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.weather_details, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(buildIntent("text/plain"));
+        return true;
+    }
+
 }
