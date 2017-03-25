d74687556a3b7ee075a0d190985d91606fa62645
egmp777 <egmp777@gmail.com>
2017-03-25 13:38:56 -0500

    Exercise S07.01
    Done

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.sunshine.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Defines table and column names for the weather database. This class is not necessary, but keeps
  * the code organized.
@@ -22,23 +24,29 @@ package com.example.android.sunshine.data;
 public class WeatherContract {
 
 //  TODO (1) Within WeatherContract, create a public static final class called WeatherEntry that implements BaseColumns
+    public static final class WeatherEntry implements BaseColumns{
+    //      Do steps 2 through 10 within the WeatherEntry class
 
-//      Do steps 2 through 10 within the WeatherEntry class
+    //      TODO (2) Create a public static final String call TABLE_NAME with the value "weather"
+    public static final String TABLE_NAME =  "weather";
 
-//      TODO (2) Create a public static final String call TABLE_NAME with the value "weather"
+    //      TODO (3) Create a public static final String call COLUMN_DATE with the value "date"
+    public static final String COLUMN_DATE = "date";
+    //      TODO (4) Create a public static final String call COLUMN_WEATHER_ID with the value "weather_id"
+    public static final String COLUMN_WEATHER_ID = "weather_id";
+    //      TODO (5) Create a public static final String call COLUMN_MIN_TEMP with the value "min"
+    public static final String COLUMN_MIN_TEMP = "min";
+    //      TODO (6) Create a public static final String call COLUMN_MAX_TEMP with the value "max"
+    public static final String COLUMN_MAX_TEMP = "max";
+    //      TODO (7) Create a public static final String call COLUMN_HUMIDITY with the value "humidity"
+    public static final String COLUMN_HUMIDITY = "humidity";
+    //      TODO (8) Create a public static final String call COLUMN_PRESSURE with the value "pressure"
+    public static final String COLUMN_PRESSURE = "pressure";
+    //      TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
+    public static final String COLUMN_WIND_SPEED = "wind";
+    //      TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
+    public static final String COLUMN_DEGREES = "degrees";
 
-//      TODO (3) Create a public static final String call COLUMN_DATE with the value "date"
+}
 
-//      TODO (4) Create a public static final String call COLUMN_WEATHER_ID with the value "weather_id"
-
-//      TODO (5) Create a public static final String call COLUMN_MIN_TEMP with the value "min"
-//      TODO (6) Create a public static final String call COLUMN_MAX_TEMP with the value "max"
-
-//      TODO (7) Create a public static final String call COLUMN_HUMIDITY with the value "humidity"
-
-//      TODO (8) Create a public static final String call COLUMN_PRESSURE with the value "pressure"
-
-//      TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
-
-//      TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
 }
--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -15,19 +15,52 @@
  */
 package com.example.android.sunshine.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Manages a local database for weather data.
  */
-// TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
-public class WeatherDbHelper {
+// COMPL TODO (11) Extend SQLiteOpenHelper from WeatherDbHelper
+public class WeatherDbHelper extends SQLiteOpenHelper {
+    // COMPL  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+    public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (12) Create a public static final String called DATABASE_NAME with value "weather.db"
+// CONPL  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
+    public static final int DATABASE_VERSION = 1;
+//  CONPL TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+    public WeatherDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-//  TODO (13) Create a private static final int called DATABASE_VERSION and set it to 1
+// COMPL TODO (15) Override onCreate and create the weather table from within it
 
-//  TODO (14) Create a constructor that accepts a context and call through to the superclass constructor
+    //  COMPL TODO (16) Override onUpgrade, but don't do anything within it yet
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-//  TODO (15) Override onCreate and create the weather table from within it
+        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
 
-//  TODO (16) Override onUpgrade, but don't do anything within it yet
+                WeatherContract.WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                WeatherContract.WeatherEntry.COLUMN_DATE + " INTEGER, " +
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + " INTEGER, "+
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + " REAL, "+
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + " REAL, "+
+                WeatherContract.WeatherEntry.COLUMN_HUMIDITY + " REAL, "+
+                WeatherContract.WeatherEntry.COLUMN_PRESSURE + " REAL, "+
+                WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + " REAL, "+
+                WeatherContract.WeatherEntry.COLUMN_DEGREES + " REAL"+
+        ");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+
 }
