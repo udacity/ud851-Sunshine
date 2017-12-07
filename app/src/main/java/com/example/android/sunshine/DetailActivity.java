09c86dce6dde5cd950488337b6cab0f9ba8f416e
Vadim <dnabyte@gmail.com>
2017-12-07 01:36:36 +0000

    Update DetailActivity.java
    Excess code

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 // COMPLETED (1) Create a new Activity called DetailActivity using Android Studio's wizard
 public class DetailActivity extends AppCompatActivity {
 
-    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
