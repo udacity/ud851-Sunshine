e6e1b9895bd75d45300b91b9a3419a8c69018511
Lorioux <magidjm@gmail.com>
2018-06-02 17:30:07 +0200

    Completed Tasks Exercise S07.03 - Table Conflict resolution

--- S07.03-Exercise-ConflictResolutionPolicy/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ S07.03-Exercise-ConflictResolutionPolicy/app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -32,7 +32,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
      */
     public static final String DATABASE_NAME = "weather.db";
 
-//  TODO (2) Increment the database version after changing the create table statement
+//  COMPLETED (2) Increment the database version after changing the create table statement
     /*
      * If you change the database schema, you must increment the database version or the onUpgrade
      * method will not be called.
@@ -44,7 +44,7 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
      * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
      * version your databases.
      */
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -85,9 +85,12 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 WeatherEntry.COLUMN_PRESSURE   + " REAL NOT NULL, "                    +
 
                 WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, "                    +
-                WeatherEntry.COLUMN_DEGREES    + " REAL NOT NULL" + ");";
+                WeatherEntry.COLUMN_DEGREES    + " REAL NOT NULL, "                    +
 
-//              TODO (1) Add a UNIQUE constraint on the date column to replace on conflict
+//              COMPLETED (1) Add a UNIQUE constraint on the date column to replace on conflict
+                "UNIQUE ( " + WeatherEntry.COLUMN_DATE + " ) ON CONFLICT REPLACE"  + ");";
+
+
 
         /*
          * After we've spelled out our SQLite table creation statement above, we actually execute
