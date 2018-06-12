c0d277a434be6b446d7c3f0a12dd032b4ba57158
Lorioux <magidjm@gmail.com>
2018-06-12 09:06:22 +0200

    Completed Tasks Exercise S10.02 -- SmarterSyncing

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -153,8 +153,8 @@ public class MainActivity extends AppCompatActivity implements
          */
         getSupportLoaderManager().initLoader(ID_FORECAST_LOADER, null, this);
 
-        //  TODO (7) Call SunshineSyncUtils's initialize method instead of startImmediateSync
-        SunshineSyncUtils.startImmediateSync(this);
+        //  COMPLETED (7) Call SunshineSyncUtils's initialize method instead of startImmediateSync
+        SunshineSyncUtils.initialize(this);
 
     }
 
--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,21 +15,41 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 
+import com.example.android.sunshine.data.WeatherContract;
 
-public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+public final class SunshineSyncUtils {
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    //  COMPLETED (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized = false;
+    //  COMPLETED (2) Create a synchronized public static void method called initialize
+    //  COMPLETED (3) Only execute this method body if sInitialized is false
+    synchronized public static void initialize(Context context) {
+        if (sInitialized) return;
+        //  COMPLETED (4) If the method body is executed, set sInitialized to true
+        //  COMPLETED (5) Check to see if our weather ContentProvider is empty
+        //  COMPLETED (6) If it is empty or we have a null Cursor, sync the weather now!
+        ContentResolver resolver = context.getContentResolver();
 
+        Cursor cursor = resolver.query(
+                WeatherContract.WeatherEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+
+        );
+        if (cursor != null || cursor.getCount()> 0 ){
+            SunshineSyncTask.syncWeather(context);
+        }
+        sInitialized = true;
+    }
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
      * execution.
