86c6bf9d144610ce29b9db5c5a8c28fe1d5a1c61
Richard Lu <rlu@jimmyjohns.com>
2018-09-26 21:55:28 -0500

    [E-23] add textview as a property of the main activity

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,10 +17,11 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    TextView weatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
