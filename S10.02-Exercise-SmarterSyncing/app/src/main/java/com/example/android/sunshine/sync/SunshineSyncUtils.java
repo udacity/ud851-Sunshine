db31d8fccb4b08d37b818188ab2d092efd870c9b
Matija <vorkapicmatija@gmail.com>
2019-07-23 15:48:08 +0200

    Optimize the synchronization process by calling it only once when the app is started and the database is empty

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -17,19 +17,50 @@ package com.example.android.sunshine.sync;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
 //  TODO (1) Declare a private static boolean field called sInitialized
-
+    private static boolean sInitialized;
     //  TODO (2) Create a synchronized public static void method called initialize
     //  TODO (3) Only execute this method body if sInitialized is false
     //  TODO (4) If the method body is executed, set sInitialized to true
     //  TODO (5) Check to see if our weather ContentProvider is empty
         //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    synchronized public static void initialize(final Context context) {
+        if (sInitialized) return;
 
+        sInitialized = true;
+
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+                String selectionStatement = WeatherContract.WeatherEntry.getSqlSelectForTodayOnwards();
+
+                Cursor cursor = context.getContentResolver().query(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        projectionColumns,
+                        selectionStatement,
+                        null,
+                        null);
+
+                if (cursor == null || cursor.getCount() == 0) {
+                    startImmediateSync(context);
+                }
+
+                cursor.close();
+                return null;
+            }
+        }.execute();
+    }
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
      * execution.
