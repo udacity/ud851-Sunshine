897b064d4eec2086dcad5210f4e8c8bedcddcc13
Matija <vorkapicmatija@gmail.com>
2019-07-23 17:27:19 +0200

    Add a FirebaseJobDispatcher service to sync weather data in the background every 3-4 hours

--- S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
+++ S10.03-Exercise-FirebaseJobDispatcher/app/src/main/java/com/example/android/sunshine/sync/SunshineFirebaseJobService.java
@@ -13,10 +13,51 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.example.android.sunshine.sync;
+
+import android.os.AsyncTask;
+
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.firebase.jobdispatcher.RetryStrategy;
 // TODO (2) Make sure you've imported the jobdispatcher.JobService, not job.JobService
 
 // TODO (3) Add a class called SunshineFirebaseJobService that extends jobdispatcher.JobService
+public class SunshineFirebaseJobService extends JobService {
 
+    private AsyncTask<Void, Void, Void> mFetchWeatherTask;
+
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+
+        mFetchWeatherTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                SunshineSyncTask.syncWeather(getApplicationContext());
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                jobFinished(job, false);
+            }
+        };
+
+        mFetchWeatherTask.execute();
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        if (mFetchWeatherTask != null) {
+            mFetchWeatherTask.cancel(true);
+        }
+        return true;
+    }
+}
 //  TODO (4) Declare an ASyncTask field called mFetchWeatherTask
 
 //  TODO (5) Override onStartJob and within it, spawn off a separate ASyncTask to sync weather data
